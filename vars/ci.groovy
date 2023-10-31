c5cc61f8c4dbbdb5b2ea9618327166a8238363e1
sunil <sunilporal713@gmail.com>
2023-10-31 06:41:42 +0530

    jenkins-file

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,53 +1,35 @@
 def call() {
     try {
-        pipeline {
+        node('workstation') {
 
-            agent {
-                label 'workstation'
+            stage('cleanup') {
+                cleanWs()
+            }
+            stage('compile/Build') {
+                common.compile()
             }
 
-            stages {
+            stage('unit tests') {
+                common.unittests()
+            }
 
-                stage('compile/Build') {
-                    steps {
-                        script {
-                            common.compile()
-                        }
-                    }
-                }
-
-                stage('unit tests') {
-                    steps {
-                        script {
-                            common.unittests()
-                        }
-                    }
-                }
-
-                    stage('quality control') {
-                        environment {
-                            SONAR_USER = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.user --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
-                            SONAR_PASS = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.pass --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
-                        }
-                        steps {
-                            sh "sonar-scanner -Dsonar.host.url=http://172.31.41.67:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=cart"
-                        }
-                    }
-
-                    stage('upload code to centralized place') {
-                        steps {
-                            echo 'upload'
-                        }
-                    }
-
+            stage('quality control') {
+                SONAR_PASS = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.pass --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
+                SONAR_USER = sh(script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.user --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+                wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]) {
+                    sh "sonar-scanner -Dsonar.host.url=http://172.31.41.67:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=cart"
                 }
             }
 
+            stage('upload code to centralized place') {
+                echo 'upload'
+            }
         }
+    }
+
     catch(Exception e) {
         common.email("failed")
     }
-
 }
 
 
