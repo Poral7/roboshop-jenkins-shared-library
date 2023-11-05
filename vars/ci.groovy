e1d20ffa49fc96edffee6d9e85c5c26128f9c9c6
sunil <poralsunil5@gmail.com>
2023-11-05 12:48:50 +0530

    jenkins-file

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,7 +1,15 @@
 def call() {
+
     if(!env.SONAR_EXTRA_OPTS) {
         env.SONAR_EXTRA_OPTS = " "
     }
+
+    if(!env.TAG_NAME) {
+        env.PUSH_CODE = "false"
+    } else {
+        env.PUSH_CODE = "true"
+    }
+
     try {
         node('workstation') {
 
@@ -27,8 +35,10 @@ def call() {
                 }
             }
 
-            stage('upload code to centralized place') {
-                echo 'upload'
+            if(env.PUSH_CODE == "true") {
+                stage('upload code to centralized place') {
+                    echo 'upload'
+                }
             }
         }
     }
