f3e6d3c4d09a3b961be8c3dd7b0a54b61a2b1679
sunil <poralsunil5@gmail.com>
2024-03-05 22:23:15 +0530

    jenkins-file

--- vars/common.groovy
+++ vars/common.groovy
@@ -43,8 +43,6 @@ def artifactpush() {
         sh "zip -r ${component}-${TAG_NAME}.zip * -x Jenkinsfile"
     }
 
-
-
     NEXUS_PASS = sh (script: 'aws ssm get-parameters --region us-east-1 --names nexus.pass --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
     NEXUS_USER = sh (script: 'aws ssm get-parameters --region us-east-1 --names nexus.user --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
     wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${NEXUS_PASS}", var: 'SECRET']]]) {
